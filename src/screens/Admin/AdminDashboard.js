1f04f392ea538b109320f407c467a7e3d4505b3d
gamal2222 <idlove857@gmail.com>
2026-02-24 02:33:12 +0200

    Create AdminDashboard.js for admin management interface

---
+++ src/screens/Admin/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <h1>Admin Management Interface</h1>
+            <p>Welcome to the Admin Dashboard!</p>
+            {/* Add your admin management functionalities here */}
+        </div>
+    );
+};
+
+export default AdminDashboard;
